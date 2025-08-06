0e027e895f4b73d7d3bfcbbb7e8cc4e0c7127586
WIN-DDSIVJIMFNF\user <amelie1908@inbox.ru>
2025-08-06 23:09:19 +0300

    Verion 1.0 - Добавление файла commands.py

---
+++ commands.py
@@ -0,0 +1,20 @@
+import telebot
+
+API_TOKEN = '8151060080:AAF5PSdJvYWfX5gDX_1cdJEEbepkSan3laY'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Привет мудила\
+""")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
