b7809902dc8436ea361561660a3f71a4bd8f7aac
WIN-DDSIVJIMFNF\user <amelie1908@inbox.ru>
2025-08-06 23:22:01 +0300

    Тест

--- commands.py
+++ commands.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Привет мудила\
+Привет мудилаffавава\
 """)
 
 
