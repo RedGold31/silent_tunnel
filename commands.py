7c7e83f182e5a912f414bc5d3aec111e06195ca0
WIN-DDSIVJIMFNF\user <amelie1908@inbox.ru>
2025-08-11 22:54:09 +0300

    Version - 1.1 - Добавление файлов database и descriptions, создание БД пользователей при использовании команды /start

--- commands.py
+++ commands.py
@@ -1,15 +1,17 @@
 import telebot
 
+from database import save_user
+from descriptions import start_descriptions
+
 API_TOKEN = '8151060080:AAF5PSdJvYWfX5gDX_1cdJEEbepkSan3laY'
 
 bot = telebot.TeleBot(API_TOKEN)
 
 
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Привет мудилаffавава\
-""")
+    save_user(message.from_user)
+    bot.reply_to(message , start_descriptions)
 
 
 @bot.message_handler(func=lambda message: True)
---
+++ database.py
@@ -0,0 +1,31 @@
+import telebot
+import sqlite3
+from datetime import datetime
+
+def save_user(user):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY,
+            username TEXT,
+            first_name TEXT,
+            last_name TEXT,
+            date_joined TEXT
+        )
+    ''')
+
+    cursor.execute('''
+        INSERT OR REPLACE INTO users (user_id, username, first_name, last_name, date_joined)
+        VALUES (?, ?, ?, ?, ?)
+    ''', (
+        user.id,
+        user.username,
+        user.first_name,
+        user.last_name,
+        datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    ))
+
+    conn.commit()
+    conn.close()
